84b06807dfe99bb97a941711484f4194e545320f
Sonny Tambiac <baguiosfinest007@gmail.com>
2024-12-04 21:45:18 +1000

    Updates

--- src/components/pages/index.js
+++
@@ -1,8 +0,0 @@
-import About from "./About";
-import Works from "./Works";
-import Playground from "./Playground";
-import Profile from "./Profile";
-import Resume from "./Resume";
-import Contact from "./Contact";
-
-export { About, Works, Playground, Profile, Resume, Contact };
